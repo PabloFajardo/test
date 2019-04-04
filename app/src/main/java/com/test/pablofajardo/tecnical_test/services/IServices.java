dcf7eadccac0ebacaf6f32e1650794e62b952a62
pablofajardo <pablo.fajardo.ar@gmail.com>
2019-04-04 02:33:57 -0300

    Use Case GetAlbum: adding models and retrofit interface

---
+++ app/src/main/java/com/test/pablofajardo/tecnical_test/services/IServices.java
@@ -0,0 +1,24 @@
+package com.test.pablofajardo.tecnical_test.services;
+
+import com.test.pablofajardo.tecnical_test.models.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public interface IServices {
+
+    String BASE_URL = "https://jsonplaceholder.typicode.com/";
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(IServices.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    @GET("albums")
+    Call<List<Album>> getAlbums();
+
+}
