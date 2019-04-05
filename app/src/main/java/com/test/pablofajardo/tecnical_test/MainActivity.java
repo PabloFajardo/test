e3580c4f54a94b1691b96c3cab04dbf717945623
pablofajardo <pablo.fajardo.ar@gmail.com>
2019-04-05 01:18:40 -0300

    Main activity adding views to support fragments

--- app/src/main/java/com/test/pablofajardo/tecnical_test/MainActivity.java
+++ app/src/main/java/com/test/pablofajardo/tecnical_test/MainActivity.java
@@ -4,57 +4,30 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.test.pablofajardo.tecnical_test.base.BaseView;
 import com.test.pablofajardo.tecnical_test.models.Album;
-import com.test.pablofajardo.tecnical_test.useCase.GetAlbumsImp;
-import com.test.pablofajardo.tecnical_test.useCase.IGetAlbums;
-
-import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BaseView.Navigation {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
-
-                AlbumsPresenter presenter = new AlbumsPresenter(new IAlbumsContract.View() {
-                    @Override
-                    public void showMessage(String message) {
-                        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-                                .setAction("Action", null).show();
-                    }
-
-                    @Override
-                    public void showAlbums(@NonNull List<Album> albumList) {
-
-                    }
-
-                });
-                presenter.getAlbums();
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-            }
-        });
+        AlbumsFragment albumsFragment = AlbumsFragment.newInstance();
+        changeFragment(albumsFragment, true);
     }
 
     @Override
@@ -77,5 +50,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void changeFragment(Fragment fragment, boolean replace) {
+
+        String tag = fragment.getClass().getSimpleName();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        if (replace){
+            ft.replace(R.id.main_frame, fragment, tag);
+            ft.addToBackStack(null);
+        }else{
+            ft.add(R.id.main_frame, fragment, tag);
+            ft.addToBackStack(tag);
+        }
+
+        ft.commit();
     }
 }
--- app/src/main/java/com/test/pablofajardo/tecnical_test/base/BaseView.java
+++ app/src/main/java/com/test/pablofajardo/tecnical_test/base/BaseView.java
@@ -1,6 +1,13 @@
 package com.test.pablofajardo.tecnical_test.base;
 
+import android.support.v4.app.Fragment;
+
 public interface BaseView {
 
     void showMessage(String message);
+
+    interface Navigation{
+
+        void changeFragment(Fragment fragment, boolean replace);
+    }
 }
